959325fe586e098c17f5b342e91fc6e0708d5ef5
DeathGodBXX <2694779216@qq.com>
2021-01-22 19:02:33 +0800

    replace myself official demo

--- pytorch_classification/Test1_official_demo/predict.py
+++ pytorch_classification/Test1_official_demo/predict.py
@@ -1,31 +1,49 @@
 import torch
-import torchvision.transforms as transforms
+from torchvision import transforms
 from PIL import Image
+
 
 from model import LeNet
 
+transform = transforms.Compose(
+    [transforms.Resize((32, 32)),
+     transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+net = LeNet()
+net.load_state_dict(torch.load('./Lenet.pth'))
+
 
 def main():
-    transform = transforms.Compose(
-        [transforms.Resize((32, 32)),
-         transforms.ToTensor(),
-         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-
     classes = ('plane', 'car', 'bird', 'cat',
                'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-    net = LeNet()
-    net.load_state_dict(torch.load('Lenet.pth'))
-
-    im = Image.open('1.jpg')
-    im = transform(im)  # [C, H, W]
-    im = torch.unsqueeze(im, dim=0)  # [N, C, H, W]
+    img = Image.open('4.jpg')
+    img = transform(img)  # 3*32*32
+    img = torch.unsqueeze(img, dim=0)  # 增加batch，原图权值是[16, 3, 5, 5]是4维的
 
     with torch.no_grad():
-        outputs = net(im)
-        predict = torch.max(outputs, dim=1)[1].data.numpy()
-    print(classes[int(predict)])
+        output = net(img)
+        predict = torch.max(output, dim=1)[1].data.numpy()
+        print("Predicted: ", classes[int(predict)])
 
 
 if __name__ == '__main__':
     main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- pytorch_classification/Test1_official_demo/train.py
+++ pytorch_classification/Test1_official_demo/train.py
@@ -1,70 +1,84 @@
 import torch
 import torchvision
-import torch.nn as nn
-from model import LeNet
-import torch.optim as optim
 import torchvision.transforms as transforms
+from model import LeNet
+from torch import nn
+from torch import optim
 
 
 def main():
+    # 下载图片同时进行的预处理
     transform = transforms.Compose(
         [transforms.ToTensor(),
-         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+    )
 
-    # 50000张训练图片
-    # 第一次使用时要将download设置为True才会自动去下载数据集
-    train_set = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                             download=False, transform=transform)
-    train_loader = torch.utils.data.DataLoader(train_set, batch_size=36,
-                                               shuffle=True, num_workers=0)
+    # 下载数据集
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                            download=False, transform=transform)
 
-    # 10000张验证图片
-    # 第一次使用时要将download设置为True才会自动去下载数据集
-    val_set = torchvision.datasets.CIFAR10(root='./data', train=False,
+    # 加载数据集,设置一次处理36个数据集
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=36,
+                                              shuffle=True, num_workers=0)
+
+    # 下载测试集
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
                                            download=False, transform=transform)
-    val_loader = torch.utils.data.DataLoader(val_set, batch_size=5000,
-                                             shuffle=False, num_workers=0)
-    val_data_iter = iter(val_loader)
-    val_image, val_label = val_data_iter.next()
-    
-    # classes = ('plane', 'car', 'bird', 'cat',
-    #            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-    net = LeNet()
-    loss_function = nn.CrossEntropyLoss()
+    # 加载数据集，批次处理5000个图片数据
+    testloader = torch.utils.data.DataLoader(testset, batch_size=10000,
+                                             shuffle=True, num_workers=0)
+
+    # 使用gpu训练,将测试集移动到gpu
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    testiter = iter(testloader)
+    test_image, test_label = testiter.next()
+    test_image = test_image.to(device)
+    test_label = test_label.to(device)
+
+    # 类别
+    # classes = ('plane', 'car', 'bird', 'cat', 'deer',
+    #            'dog', 'frog', 'horse', 'ship', 'truck')
+
+    # 训练模型移动到gpu上
+    net = LeNet().to(device)
+    criterion = nn.CrossEntropyLoss().to(device)
     optimizer = optim.Adam(net.parameters(), lr=0.001)
 
-    for epoch in range(5):  # loop over the dataset multiple times
+    for epoch in range(5):
+        """每次epoch，50000/36约等于1000，每次训练结果就是500+500，500打印一次"""
 
         running_loss = 0.0
-        for step, data in enumerate(train_loader, start=0):
-            # get the inputs; data is a list of [inputs, labels]
-            inputs, labels = data
+        for step, data in enumerate(trainloader, start=0):
+            """一次处理36个图片，枚举遍历所有下载的trainset"""
 
-            # zero the parameter gradients
+            # data is a list of [inputs,labels]
+            inputs, labels = data[0].to(device), data[1].to(device)
+
+            # zero the optimizer grad
             optimizer.zero_grad()
-            # forward + backward + optimize
+
+            # forward + loss + backward + optimizer
             outputs = net(inputs)
-            loss = loss_function(outputs, labels)
+            loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
 
-            # print statistics
             running_loss += loss.item()
-            if step % 500 == 499:    # print every 500 mini-batches
-                with torch.no_grad():
-                    outputs = net(val_image)  # [batch, 10]
-                    predict_y = torch.max(outputs, dim=1)[1]
-                    accuracy = torch.eq(predict_y, val_label).sum().item() / val_label.size(0)
 
+            if step % 500 == 499:
+                with torch.no_grad():
+                    outputs = net(test_image)
+                    # test_result = torch.max(outputs, dim=1)[1]
+                    test_result = torch.max(torch.softmax(outputs, dim=1), dim=1)[1]
+                    accuracy = torch.eq(test_result, test_label).sum().item() / test_label.size(0)
                     print('[%d, %5d] train_loss: %.3f  test_accuracy: %.3f' %
                           (epoch + 1, step + 1, running_loss / 500, accuracy))
                     running_loss = 0.0
 
-    print('Finished Training')
+    print("Finished Training!!!")
 
-    save_path = './Lenet.pth'
-    torch.save(net.state_dict(), save_path)
+    torch.save(net.state_dict(), './Lenet.pth')
 
 
 if __name__ == '__main__':
